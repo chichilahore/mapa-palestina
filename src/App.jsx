9d708137434a150f60a008465dd802a019c3e4c7
chichilahore <alexandrchichiloza@gmail.com>
2025-07-26 19:30:56 +0200

    Initial commit: proyecto mapa palestina con React y Leaflet

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import WorldMap from './components/worldMap/worldMap'
+
+function App() {
+  return <WorldMap />
+}
+
+export default App
---
+++ src/components/worldMap/worldMap.jsx
@@ -0,0 +1,19 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const WorldMap = () => {
+  return (
+    <MapContainer
+      center={[20, 0]}
+      zoom={2}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+    </MapContainer>
+  );
+};
+
+export default WorldMap;
